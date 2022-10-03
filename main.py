0c9449b73b03bfc3fdffa538d9439cd6750d39d6
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2022-10-03 16:54:15 +0200

    prepare refactor

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import requests
 from rdflib import Graph, URIRef, Literal, BNode
 from rdflib.namespace import  RDF
 
+def sources(name):
+    g = Graph()
+    g = dblp(name, g)
+    g = zenodo(name, g)
+    # TODO add materialized triples via https://github.com/RDFLib/OWL-RL
+    g.parse('zenodo2schema.ttl')
+    return g.serialize(format="turtle")
+
 
 # rtldW8mT6PgLkj6fUL46nu02YQaUGYfGT8FjuoJMTK4gdwizDLyt6foRVaGL access token zenodo
 def extract_metadata(text):
@@ -25,29 +33,9 @@ def extract_metadata(text):
                                          'opengraph'])
     return metadata
 
-def sources(name):
-    g = Graph()
-    g = dblp(name, g)
-    g = zenodo(name, g)
-    # TODO add materialized triples via https://github.com/RDFLib/OWL-RL
-    g.parse('zenodo2schema.ttl')
-    return g.serialize(format="turtle")
 
 
-def zenodo(name, g):
-    response = requests.get('https://zenodo.org/api/records',
-                            params={'q': name,
-                                    'access_token': 'rtldW8mT6PgLkj6fUL46nu02YQaUGYfGT8FjuoJMTK4gdwizDLyt6foRVaGL'})
-    for data in response.json()['hits']['hits']:
-        if('conceptdoi' in data):
-            # TODO Align and extend with schema.org concepts
-            subject = URIRef(data['conceptdoi'])
-            object = URIRef('zenodo:'+data['metadata']['resource_type']['type'])
-            g.add((subject, RDF.type, object))
-        if('conceptrecid' in data):
-            # TODO parse this
-            print('TODO')
-    return g
+
 
 def dblp(name, g):
     headers = {'Accept': 'application/json'}
@@ -68,6 +56,20 @@ def dblp(name, g):
 
     return g
 
+def zenodo(name, g):
+    response = requests.get('https://zenodo.org/api/records',
+                            params={'q': name,
+                                    'access_token': 'rtldW8mT6PgLkj6fUL46nu02YQaUGYfGT8FjuoJMTK4gdwizDLyt6foRVaGL'})
+    for data in response.json()['hits']['hits']:
+        if('conceptdoi' in data):
+            # TODO Align and extend with schema.org concepts
+            subject = URIRef(data['conceptdoi'])
+            object = URIRef('zenodo:'+data['metadata']['resource_type']['type'])
+            g.add((subject, RDF.type, object))
+        if('conceptrecid' in data):
+            # TODO parse this
+            print('TODO')
+    return g
 
 demo = gr.Interface(fn=sources, inputs="text", outputs="text")
 
