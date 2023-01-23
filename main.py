cec462d9ec6997a46ea45a6ad883c7db2a8bdc63
Patrick Westphal <patrick@Patricks-MacBook-Pro.local>
2023-01-23 11:50:41 +0100

    Improve readabiliy

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import json
 import logging
+from typing import List
 
 import extruct
 import gradio as gr
 import requests
-from rdflib import Graph, URIRef, Literal, BNode, Namespace
-from rdflib.namespace import RDF, FOAF, DCTERMS, XSD, SDO
+from rdflib import Graph, URIRef, RDF
 from objects import Person, Zenodo, Article
-from search_openalex import *
+import search_openalex
 
 logging.config.fileConfig(os.getenv('LOGGING_FILE_CONFIG', './logging.conf'))
 logger = logging.getLogger('nfdi_search_engine')
 
 
-def sources(name):
+def sources(search_term):
     g = Graph()
     results = []
     g, results = dblp(name, g, results)
     g, results = zenodo(name, g, results)
     g, results  = open_alex(name, g, results)
+
     # TODO add materialized triples via https://github.com/RDFLib/OWL-RL
     g.parse('zenodo2schema.ttl')
     g = g.serialize(format="ttl")
@@ -28,10 +29,10 @@ def sources(name):
 
 
 def format_results(results):
-    """From the results obtained from the databases it creates an HTML, and it returns it
+    """From the results obtained from the databases it creates an HTML page, and it returns it
 
         Args:
-            results: array of the different types of results
+            results: List of the different results
 
         Returns:
             string: single string with the HTML of the results
@@ -49,39 +50,58 @@ def format_results(results):
                             </style>
                             </head>
                             <body>"""
-    person_result = "<h1 class='title'>Researchers - </h1><span class='emoji-title'>&#129417;</span><br><br>"
+
+    person_result = "<h1 class='title'>Researchers - </h1>" \
+                    "<span class='emoji-title'>&#129417;</span>" \
+                    "<br><br>"
     exist_person = False
-    article_result = "<h1 class='title'>Articles - </h1><span class='emoji-title'>&#128240;</span><br><br>"
+
+    article_result = "<h1 class='title'>Articles - </h1>" \
+                     "<span class='emoji-title'>&#128240;</span>" \
+                     "<br><br>"
     exist_article = False
-    zenodo_result = "<h1 class='title'>Zenodo - </h1><span class='emoji-title'>&#128188;</span><br><br>"
+
+    zenodo_result = "<h1 class='title'>Zenodo - </h1>" \
+                    "<span class='emoji-title'>&#128188;</span>" \
+                    "<br><br>"
     exist_zenodo = False
+
     for result in results:
-        if type(result) is Person:
-            person_result += "<span class='emoji'>&#129417;</span><h2 class='subtitle'><a href='"+result.URL+"' target='_blank' >"+result.name + \
-                                 "</a></h2><p class='faded'>"+result.URL+"</p><br>"
+        if isinstance(result, Person):
+            person_result += \
+                f"<span class='emoji'>&#129417;</span>" \
+                f"<h2 class='subtitle'><a href='{result.url}' target='_blank' >{result.name}</a></h2>" \
+                f"<p class='faded'>{result.url}</p><br>"
             exist_person = True
-        elif type(result) is Article:
-            if "," in result.URL:
-                article_result += "<p class='url'>" + result.URL \
-                                     + "</p><span class='emoji'>&#128240;</span><h2 class='subtitle'></i><a href='" +\
-                                     result.URL.split(",")[0] + "' target='_blank'>" + \
-                                     result.title + "</a></h2> - " + result.date + "<p>" + result.authors + "</p><br>"
+
+        elif isinstance(result, Article):
+            if "," in result.url:
+                url = result.url.split(',')[0]
             else:
-                article_result += "<p class='url'>" + result.URL +\
-                                     "</p><span class='emoji'>&#128240;</span><h2 class='subtitle'><a href='"+result.URL+\
-                                     "' target='_blank'>" + result.title + \
-                                     "</a></h2> - "+result.date+"<p>"+result.authors+"</p><br>"
+                url = result.url
+
+            article_result +=\
+                f"<p class='url'>{result.url}</p>" \
+                f"<span class='emoji'>&#128240;</span>" \
+                f"<h2 class='subtitle'><a href='{url}' target='_blank'>{result.title}</a></h2>" \
+                f" - {result.date}" \
+                f"<p>{result.authors}</p>" \
+                f"<br>"
             exist_article = True
-        elif type(result) is Zenodo:
-            zenodo_result += "<p class='url'>" + result.URL +\
-                                 "</p><span class='emoji'>&#128188;</span><h2 class='subtitle'><a href='"+\
-                                 result.URL+"' target='_blank'>" + result.title + \
-                                 "</a></h2> - "+result.date+ "<p>" + result.type + "</p><br>"
+
+        elif isinstance(result, Zenodo):
+            zenodo_result +=\
+                f"<p class='url'>{result.url}</p>" \
+                f"<span class='emoji'>&#128188;</span>" \
+                f"<h2 class='subtitle'><a href='{result.url}' target='_blank'>{result.title}</a></h2>" \
+                f" - {result.date}" \
+                f"<p>{result.resource_type}</p>" \
+                f"<br>"
             exist_zenodo = True
         else:
             logger.warning(f"Type {type(result)} of result not yet handled")
 
-    #Checking that we have actual results and act accordingly if not
+    # Checking that we have actual results and act accordingly if not
     if not exist_person:
         person_result = ""
     if not exist_article:
@@ -90,19 +110,21 @@ def format_results(results):
         zenodo_result = ""
     if not exist_zenodo and not exist_article and not exist_person:
         person_result = "<br><br><h1>No results found </h1><span class='emoji-title'>&#128549;</span>"
+
     return formatted_results + person_result + article_result + zenodo_result + "</body></html>"
 
+
 # rtldW8mT6PgLkj6fUL46nu02YQaUGYfGT8FjuoJMTK4gdwizDLyt6foRVaGL access token zenodo
-def extract_metadata(text):
+def extract_metadata(text: bytes):
     """Extract all metadata present in the page and return a dictionary of metadata lists.
 
     Args:
+        text: The content of a requests.get( ) call
 
     Returns:
         metadata (dict): Dictionary of json-ld, microdata, and opengraph lists.
         Each of the lists present within the dictionary contains multiple dictionaries.
     """
-
     metadata = extruct.extract(text,
                                uniform=True,
                                syntaxes=['json-ld',
@@ -111,37 +133,128 @@ def extract_metadata(text):
     return metadata
 
 
-
-def dblp(name, g, results):
+def dblp(search_term: str, g: Graph, results: List):
     headers = {'Accept': 'application/json'}
 
-    response = requests.get('https://dblp.uni-trier.de/search?q=' + name, headers=headers)
+    response = requests.get(
+        'https://dblp.uni-trier.de/search?q=' + search_term,
+        headers=headers
+    )
 
     logger.debug(f'DBLP response status code: {response.status_code}')
     logger.debug(f'DBLP response headers: {response.headers}')
 
     # TODO unclear why here are only a few but now all results returned
+
     metadata = extract_metadata(response.content)
 
     # TODO unclear why this loop takes so long
+    # PW:
+    # The profiler says:
+    #          3188239 function calls (3012651 primitive calls) in 18.210 seconds
+    #
+    #    Ordered by: cumulative time
+    #
+    #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+    #        30    0.000    0.000   18.210    0.607 graph.py:1223(parse)
+    #        30    0.008    0.000   18.202    0.607 jsonld.py:81(parse)
+    #        30    0.000    0.000   18.189    0.606 jsonld.py:118(to_rdf)
+    #        30    0.019    0.001   18.189    0.606 jsonld.py:146(parse)
+    #        30    0.000    0.000   18.107    0.604 context.py:365(load)
+    #     60/30    0.001    0.000   16.618    0.554 context.py:390(_prep_sources)
+    #        30    0.001    0.000   16.615    0.554 context.py:430(_fetch_context)
+    #        60    0.002    0.000   16.615    0.277 util.py:27(source_to_json)
+    #        60    0.001    0.000   13.612    0.227 parser.py:332(create_input_source)
+    #        30    0.000    0.000   13.611    0.454 parser.py:436(_create_input_source_from_location)
+    #        30    0.003    0.000   13.608    0.454 parser.py:231(__init__)
+    # ...
+    #
+    # I.e. the JSON-LD parsing takes that long
     for data in metadata['microdata']:
         if data['@type'] == 'Person':
-            results.append(Person(data["name"], data["url"]))
+            # E.g:
+            # {
+            #   '@type': 'Person',
+            #   '@context': 'http://schema.org',
+            #   'additionalType': 'https://dblp.org/rdf/schema#Person',
+            #   'url': 'https://dblp.uni-trier.de/pid/65/9656',
+            #   'name': 'Ricardo Usbeck'
+            # }
+            results.append(
+                Person(
+                    name=data["name"],
+                    url=data["url"]
+                )
+            )
+
             g.parse(data=json.dumps(data), format='json-ld')
-        if data['@type'] == 'ScholarlyArticle':
-            author = ""
+
+        elif data['@type'] == 'ScholarlyArticle':
+            # E.g.
+            # {
+            #   '@type': 'ScholarlyArticle',
+            #   '@context': 'http://schema.org',
+            #   'additionalType': 'https://dblp.org/rdf/schema#Publication',
+            #   'image': 'https://dblp.uni-trier.de/img/paper-oa.dark.hollow.16x16.png',
+            #   'url': 'https://doi.org/10.1109/ACCESS.2022.3173355',
+            #   'headline': 'Md. Rashad Al Hasan Rony, ...',
+            #   'author':
+            #     [
+            #       {
+            #         '@type': 'Person',
+            #         'url': 'https://dblp.uni-trier.de/pid/251/0778.html',
+            #         'name': 'Md. Rashad Al Hasan Rony'
+            #       },
+            #       {
+            #         '@type': 'Person',
+            #         'url': 'https://dblp.uni-trier.de/pid/213/7337.html',
+            #         'name': 'Debanjan Chaudhuri'
+            #       },
+            #       {
+            #         '@type': 'Person',
+            #         'url': 'https://dblp.uni-trier.de/pid/65/9656.html',
+            #         'name': 'Ricardo Usbeck'
+            #       },
+            #       {
+            #         '@type': 'Person',
+            #         'url': 'https://dblp.uni-trier.de/pid/71/4882.html',
+            #         'name': 'Jens Lehmann'
+            #       }
+            #     ],
+            #   'name': 'Tree-KGQA: An Unsupervised Approach for Question...',
+            #   'isPartOf':
+            #     [
+            #       {
+            #         '@type': 'Periodical',
+            #         'name': 'IEEE Access'
+            #       },
+            #       {
+            #         '@type': 'PublicationVolume',
+            #         'volumeNumber': '10'
+            #       }
+            #     ],
+            #   'pagination': '50467-50478',
+            #   'datePublished': '2022'
+            # }
             if type(data["author"]) == list:
-                author = ','.join([authors["name"] for authors in data["author"]])
+                author = ', '.join([authors["name"] for authors in data["author"]])
             else:
                 author = data["author"]
 
-            url = ""
             if type(data["url"]) == list:
-                url = ','.join(data["url"])
+                url = ', '.join(data["url"])
             else:
                 url = data["url"]
 
-            results.append(Article(data["name"], url, author, data["datePublished"]))
+            results.append(
+                Article(
+                    title=data["name"],
+                    url=url,
+                    authors=author,
+                    date=data["datePublished"]
+                )
+            )
+
             g.parse(data=json.dumps(data), format='json-ld')
     logger.info(f"Graph g has {len(g)} statements after querying DBLP.")
 
@@ -149,9 +262,9 @@ def dblp(name, g, results):
     return g, results
 
 
-def zenodo(name, g, results):
+def zenodo(search_term, g, results):
     response = requests.get('https://zenodo.org/api/records',
-                            params={'q': name,
+                            params={'q': search_term,
                                     'access_token': 'rtldW8mT6PgLkj6fUL46nu02YQaUGYfGT8FjuoJMTK4gdwizDLyt6foRVaGL'})
 
     logger.debug(f'Zenodo response status code: {response.status_code}')
@@ -160,10 +273,18 @@ def zenodo(name, g, results):
     for data in response.json()['hits']['hits']:
         if 'conceptdoi' in data:
             # TODO Align and extend with schema.org concepts
-            subject = URIRef(data['conceptrecid'])
-            object = URIRef('zenodo:' + data['metadata']['resource_type']['type'])
-            g.add((subject, RDF.type, object))
-            results.append(Zenodo(subject, object, data["links"]["doi"], data['metadata']['publication_date'], data['metadata']['title']))
+            resource = URIRef(data['conceptrecid'])
+            resource_type = URIRef('zenodo:' + data['metadata']['resource_type']['type'])
+            g.add((resource, RDF.type, resource_type))
+            results.append(
+                Zenodo(
+                    uri=resource,
+                    resource_type=resource_type,
+                    url=data["links"]["doi"],
+                    date=data['metadata']['publication_date'],
+                    title=data['metadata']['title']
+                )
+            )
 
     logger.info(f"Graph g has {len(g)} statements after querying Zenodo.")
     return g, results
@@ -229,9 +350,11 @@ with gr.Blocks() as demo:
             html = gr.HTML("<h1 style = 'font-size: 20px;'>Search Results</h1>")
     search.click(sources, input_text, html)
 
+
 def open_alex(name, g, results):
-    serializable_results, results = find(name, results)
+    serializable_results = search_openalex.find(name, results)
     search_result = json.dumps(serializable_results)
+
     g.parse(data=search_result, format='json-ld')
     return g, results
 
--- search_openalex.py
+++ search_openalex.py
@@ -1,23 +1,32 @@
 import logging
+from typing import List
 
 from diophila import OpenAlex
-import json
 
 from objects import Person, Article
 
 logger = logging.getLogger('nfdi_search_engine')
 
 
-def find(search_key, results):
+def find(search_key: str, results: List):
     oa = OpenAlex()
-    search_result = {
+    search_results_json_ld = {
         "@context": "http://schema.org/"
     }
     works_search_result, results = find_works(oa, search_key, results)
     authors_search_result, results = find_authors(oa, search_key, results)
     api_response_concepts = oa.get_list_of_concepts(search=search_key)
-    search_result.update({"@graph": [{"graph1": works_search_result}, {"graph2": authors_search_result}]})
-    return search_result, results
+    search_results_json_ld.update(
+        {
+            "@graph":
+                [
+                    {"graph1": works_search_result},
+                    {"graph2": authors_search_result}
+                ]
+        }
+    )
+
+    return search_results_json_ld
 
 
 def find_authors(oa, search_key, results):
@@ -25,23 +34,69 @@ def find_authors(oa, search_key, results):
     list_of_authors = []
     for authors in api_response_authors:
         for author in authors['results']:
+            # E.g.:
+            #  {
+            #    "id": "https://openalex.org/A4222267058",
+            #    "orcid": None,
+            #    "display_name": "Ricardo Usbeck",
+            #    "display_name_alternatives": [],
+            #    "relevance_score": 184.41525,
+            #    "works_count": 1,
+            #    "cited_by_count": 4,
+            #    "ids": {
+            #      "openalex": "https://openalex.org/A4222267058"
+            #    },
+            #    "x_concepts": [
+            #      ...
+            #    ],
+            #    "counts_by_year": [
+            #      {
+            #        "year": 2022,
+            #        "works_count": 1,
+            #        "cited_by_count": 4
+            #      }
+            #    ],
+            #    "works_api_url": "https://api.openalex.org/works?filter=author.id:A4222267058",
+            #    "updated_date": "2023-01-18T18:01:48.538514",
+            #    "created_date": "2022-04-05"
+            # }
             if 'id' in author:
                 concepts = []
                 for concept in author['x_concepts']:
+                    # E.g.:
+                    # {
+                    #   "id": "https://openalex.org/C23123220",
+                    #   "wikidata": "https://www.wikidata.org/wiki/Q816826",
+                    #   "display_name": "Information retrieval",
+                    #   "level": 1,
+                    #   "score": 100
+                    # }
                     concepts.append(
-                        {'@id': concept['id'], 'sameAs': concept['wikidata'], 'name': concept['display_name']})
+                        {
+                            '@id': concept['id'],
+                            'sameAs': concept['wikidata'],
+                            'name': concept['display_name']
+                        }
+                    )
 
                 list_of_authors.append({
-                    '@id': author['id'], '@type': 'Person', 'name': author['display_name'], 'ids': author['ids'],
+                    '@id': author['id'],
+                    '@type': 'Person',
+                    'name': author['display_name'],
+                    'ids': author['ids'],
                     'works_count': author['works_count'],
                     'cited_by_count': author['cited_by_count'],
                     'last_known_institution': author['last_known_institution'],
                     'x_concepts': concepts,
                     'works_api_url': author['works_api_url']
                 })
-                if author['display_name'] is None or author['id'] is None:
-                    continue
-                results.append(Person(author['display_name'], author['id']))
+                if author['display_name'] and author['id']:
+                    results.append(
+                        Person(
+                            name=author['display_name'],
+                            url=author['id']
+                        )
+                    )
 
         logger.info(f'Got {len(list_of_authors)} author records from OpenAlex')
         return list_of_authors, results
@@ -56,14 +111,24 @@ def find_works(oa, search_key, results):
             if 'id' in work:
                 list_of_works.append(parse_to_jsonld(work))
 
-                if work["display_name"] is None or work["id"] is None or work["publication_year"] is None:
+                if work["display_name"] is None \
+                        or work["id"] is None \
+                        or work["publication_year"] is None:
                     continue
 
                 if len(work["authorships"]) == 1:
                     author = work["authorships"][0]["author"]["display_name"]
                 else:
-                    author = ','.join(current_author["author"]["display_name"] for current_author in work["authorships"])
-                results.append(Article(work["display_name"], work["id"], author, str(work["publication_year"])))
+                    author = ', '.join(current_author["author"]["display_name"] for current_author in work["authorships"])
+
+                results.append(
+                    Article(
+                        title=work["display_name"],
+                        url=work["id"],
+                        authors=author,
+                        date=str(work["publication_year"])
+                    )
+                )
 
     logger.info(f'Got {len(list_of_works)} publication records from OpenAlex')
     return list_of_works, results
@@ -93,7 +158,7 @@ def get_authorship_information(work_authors):
     return authors_info
 
 
-def parse_to_jsonld(work: object) -> dict:
+def parse_to_jsonld(work: dict) -> dict:
     host_venue = work['host_venue']
     display_name = work['display_name']
     host_information = [{'@id': host_venue['id'], '@type': host_venue['type'],
@@ -104,7 +169,7 @@ def parse_to_jsonld(work: object) -> dict:
     for concept in work['concepts']:
         concepts.append({'@id': concept['id'], 'sameAs': concept['wikidata'], 'name': concept['display_name']})
 
-    temp = {
+    return {
         '@id': work['id'], '@type': 'article', 'title': work['title'], 'doi': work['doi'],
         'publication_date': work['publication_date'],
         'host_venue': host_information, 'authors': author_information,
@@ -113,4 +178,3 @@ def parse_to_jsonld(work: object) -> dict:
         'referenced_works': work['referenced_works'],
         'related_works': work['related_works']
     }
-    return temp
