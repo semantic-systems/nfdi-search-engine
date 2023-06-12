e12fed109bab80b35f7624ab0571ea83542d8d5f
Jan Reineke <andorra97@gmail.com>
2023-06-12 16:48:28 +0200

    Feature: #39 - Wikidata-Search - Part2. Added Search for Persons; Query improvements

--- search_wikidata.py
+++ search_wikidata.py
@@ -21,7 +21,7 @@ def wikidata_search(search_string: str, results):
       """
 
     wikidata_article_search(search_string, results)
-    # wikidata_person_search(search_string, results)
+    wikidata_person_search(search_string, results)
 
     logger.info(f"Got {len(results)} author and publication records from Wikidata")
     return results
@@ -29,16 +29,17 @@ def wikidata_search(search_string: str, results):
 
 def wikidata_article_search(search_string: str, results):
     url = 'https://query.wikidata.org/sparql'
-    headers = {'User-Agent': 'NFDI4DS-Research'}
+    headers = {'User-Agent': 'https://nfdi-search.nliwod.org/'}
     query_template = Template('''
-SELECT DISTINCT ?item ?label (year(?date)as ?dateYear) (group_concat(DISTINCT ?authors2; separator=", ") as ?authorsString) ?authorsLabel
-
+ SELECT DISTINCT ?item ?label (year(?date)as ?dateYear)
+(group_concat(DISTINCT ?authorsName; separator=", ") as ?authorsLabel)
+(group_concat(DISTINCT ?authors2; separator=", ") as ?authorsString) 
   WHERE
   {
     SERVICE wikibase:mwapi
     {
       bd:serviceParam wikibase:endpoint "www.wikidata.org";
-                      wikibase:limit 100;
+                      wikibase:limit 1000;
                       wikibase:api "Generator";
                       mwapi:generator "search";
                       mwapi:gsrsearch "$search_string";
@@ -46,16 +47,13 @@ SELECT DISTINCT ?item ?label (year(?date)as ?dateYear) (group_concat(DISTINCT ?a
       ?item wikibase:apiOutputItem mwapi:title.
     }
     ?item rdfs:label ?label. FILTER( LANG(?label)="en" )
-
     ?item wdt:P31/wdt:P279* wd:Q11826511.
-    #wd:Q11826511 ^wdt:P279*/^wdt:P31 ?item.
     ?item wdt:P577 ?date .
     ?item wdt:P50 ?authors.
-    ?item wdt:P2093 ?authors2.
-    
-SERVICE wikibase:label {bd:serviceParam wikibase:language "en" .}
+    ?authors rdfs:label ?authorsName . FILTER( LANG(?authorsName)="en" )
+    optional {?item wdt:P2093 ?authors2.}
   }
-GROUP BY ?item ?label ?date ?authorsLabel
+GROUP BY ?item ?label ?date 
 ORDER BY DESC(?dateYear)
   ''')
 
@@ -65,22 +63,69 @@ ORDER BY DESC(?dateYear)
     logger.debug(f'DBLP response status code: {response.status_code}')
     logger.debug(f'DBLP response headers: {response.headers}')
 
-    if response.status_code != 404 and response.status_code != 408 and response.status_code != 504:
+    if response.status_code == 200:
         data = response.json()
         if data["results"]["bindings"]:
+            pd.options.mode.chained_assignment = None  # default='warn'
             result_df = pd.json_normalize(data['results']['bindings'])
             df = result_df[['item.value', 'label.value', 'dateYear.value', 'authorsLabel.value', 'authorsString.value']]
             df.columns = ['url', 'title', 'date', 'authors', 'authors2']
-            df = df.groupby(['url', 'date', 'title', 'authors2'])["authors"].apply(lambda x: ", ".join(x)).reset_index()
-            df['allAuthors'] = df['authors'].map(str) + ', ' + df['authors2'].map(str)
+            df['allAuthors'] = df['authors'] + ', ' + df['authors2']
             df = df.sort_values(by=['date'], ascending=False).reset_index()
             df_dict = df.to_dict('records')
-
             for row in df_dict:
                 results.append(Article(
                     title=row['title'],
                     url=row['url'],
-                    authors=row['allAuthors'],
+                    authors=str.strip(row['allAuthors'], ", "),
                     description='',
                     date=row['date']
                 ))
+
+def wikidata_person_search(search_string: str, results):
+    url = 'https://query.wikidata.org/sparql'
+    headers = {'User-Agent': 'https://nfdi-search.nliwod.org/'}
+    query_template = Template('''
+    SELECT DISTINCT ?item ?label ?employer ?employerLabel
+  WHERE
+  {
+    SERVICE wikibase:mwapi
+    {
+      bd:serviceParam wikibase:endpoint "www.wikidata.org";
+                      wikibase:api "Generator";
+                      mwapi:generator "search";
+                      mwapi:gsrsearch "$search_string";
+                      mwapi:gsrlimit "150".
+      ?item wikibase:apiOutputItem mwapi:title.
+    }
+    ?item rdfs:label ?label. FILTER( LANG(?label)="en" )
+
+    # ?item wdt:P31/wdt:P279* wd:Q5 .
+    ?item wdt:P106 ?occ .
+    ?occ wdt:P279* wd:Q482980 .
+   SERVICE wikibase:label {
+     bd:serviceParam wikibase:language "en" .
+  }
+  }
+      ''')
+
+    response = requests.get(url,
+                            params={'format': 'json', 'query': query_template.substitute(search_string=search_string),
+                                    }, timeout=(3, 15), headers=headers)
+    logger.debug(f'DBLP response status code: {response.status_code}')
+    logger.debug(f'DBLP response headers: {response.headers}')
+
+    if response.status_code == 200:
+        data = response.json()
+        if data["results"]["bindings"]:
+            result_df = pd.json_normalize(data['results']['bindings'])
+            df = result_df[['item.value', 'label.value']]
+            df.columns = ['url','name']
+            df_dict = df.to_dict('records')
+
+            for row in df_dict:
+                results.append(Person(
+                    name = row['name'],
+                    url = row['url'],
+                    affiliation = ''
+               ))
