b3ea4f8e5fb3301a30662f793c560379f9a38127
Meisenburger13 <melissa.eisenburger@gmail.com>
2023-06-23 13:08:26 +0200

    links from wikidata get added
    it's quite slow, because they don't have the whole url saved and I need to look up the formatter urls for all of the codes.

--- details_page.py
+++ details_page.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from string import Template
 
 import requests
@@ -15,22 +16,22 @@ def search_openalex(search_term: str):
     if search_term.startswith('https://orcid.org/'):
         api_url += 'authors/'
     api_url_author = requests.get(api_url + search_term)
-    if api_url_author.status_code != 404:
-        author = api_url_author.json()
-        links.append(author['id'])
-        if author['orcid']:
-            links.append(author['orcid'])
-        if 'wikipedia' in author['ids'].keys():
-            links.append(author['wikipedia'])
-        if 'twitter' in author['ids'].keys():
-            links.append(author['twitter'])
-        if author['last_known_institution']:
-            inst = author['last_known_institution']
-            details['Last known institution'] = inst['display_name'] + ' (' + inst['country_code'] + ')'
-        details['Number of works'] = author['works_count']
-        details['Number of citations'] = author['cited_by_count']
-        return details, links, author['display_name']
-    return details, links, ''
+    if api_url_author.status_code != 200:
+        return details, links, ''
+    author = api_url_author.json()
+    links.append(author['id'])
+    if author['orcid']:
+        links.append(author['orcid'])
+    if 'wikipedia' in author['ids'].keys():
+        links.append(author['wikipedia'])
+    if 'twitter' in author['ids'].keys():
+        links.append(author['twitter'])
+    if author['last_known_institution']:
+        inst = author['last_known_institution']
+        details['Last known institution'] = inst['display_name'] + ' (' + inst['country_code'] + ')'
+    details['Number of works'] = author['works_count']
+    details['Number of citations'] = author['cited_by_count']
+    return details, links, author['display_name']
 
 
 def search_dblp(search_term: str):
@@ -46,7 +47,8 @@ def search_dblp(search_term: str):
 
     details = {}
     links = []
-
+    if not ET.fromstring(response):
+        return details, links, ''
     author = ET.fromstring(response)[0]
     name = author.find('.author').text
     links.append("https://dblp.uni-trier.de/pid/" + author.find('.author').attrib['pid'])
@@ -64,7 +66,7 @@ def get_orcid(search_term):
             author = api_url_author.json()
             if author['orcid']:
                 return author['orcid']
-    if search_term.startswith('https://dblp'):
+    elif search_term.startswith('https://dblp'):
         headers = {'Accept': 'application/json'}
         response = requests.get(
             search_term + '.xml',
@@ -74,7 +76,7 @@ def get_orcid(search_term):
         for url in author.findall('.url'):
             if url.text.startswith('https://orcid.org/'):
                 return url.text
-    if search_term.startswith('http://www.wikidata'):
+    elif search_term.startswith('http://www.wikidata'):
         url = search_term
         headers = {'User-Agent': 'https://nfdi-search.nliwod.org/'}
         response = requests.get(url, headers=headers)
@@ -111,16 +113,18 @@ def search_wikidata(search_term):
 
         wikidata_id = search_term.split('/')[-1]
 
-        if response.status_code == 200:
-            data = response.json()
-            author = (data['entities'][wikidata_id])
-            name = author.get('labels').get('en').get('value')
-            if 'en' in author.get('descriptions').keys():
-                details['Description'] = author.get('descriptions').get('en').get('value')
-            links.append('https://www.wikidata.org/entity/' + wikidata_id)
+        if response.status_code != 200:
             return details, links, name
-
+        data = response.json()
+        author = (data['entities'][wikidata_id])
+        name = author.get('labels').get('en').get('value')
+        if 'en' in author.get('descriptions').keys():
+            details['Description'] = author.get('descriptions').get('en').get('value')
+        links.append('https://www.wikidata.org/entity/' + wikidata_id)
+        links_dict = author['claims']
+        links += get_wiki_links(links_dict)
         return details, links, name
+
     elif search_term.startswith('https://orcid.org/'):
         orcid = search_term.split('/')[-1]
         url = 'https://query.wikidata.org/sparql'
@@ -141,14 +145,28 @@ def search_wikidata(search_term):
                                 params={'format': 'json',
                                         'query': query_template.substitute(orcid=orcid),
                                         }, timeout=(3, 15), headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            wikidata_link = ''
-            match = data['results']['bindings']
-            if match:
-                wikidata_link = match[0]['item']['value']
-                return search_wikidata(wikidata_link)
+        if response.status_code != 200:
+            return details, links, name
+        data = response.json()
+        wikidata_link = ''
+        match = data['results']['bindings']
+        if match:
+            wikidata_link = match[0]['item']['value']
+            return search_wikidata(wikidata_link)
     return details, links, name
 
-# dblp: P2456
-# orcid: P496
+def get_wiki_links(links_dict):
+    links = []
+    headers = {'User-Agent': 'https://nfdi-search.nliwod.org/'}
+    for prop_code in links_dict.keys():
+        url = 'https://www.wikidata.org/wiki/Special:EntityData/' + prop_code
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            continue
+        data = response.json()
+        claims = data['entities'][prop_code]['claims']
+        end_of_link = links_dict[prop_code][0]['mainsnak']['datavalue']['value']
+        if 'P1630' in claims.keys() and isinstance(end_of_link, str):
+            formatter_link = claims['P1630'][0]['mainsnak']['datavalue']['value']
+            links.append(formatter_link.replace('$1', end_of_link))
+    return links
