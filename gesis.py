79b231e9646ddb58cdb94febbf640ae1ee07fe16
Najeeb Shams <44796050+Najeeb-Shams@users.noreply.github.com>
2023-07-12 16:50:41 +0200

    GESIS and ORCID updated

--- gesis.py
+++ gesis.py
@@ -8,67 +8,84 @@ import utils
 
 logger = logging.getLogger('nfdi_search_engine')
 
+
 def search(search_term, results):
-
+    # API URL with the search term
     api_url = f'http://193.175.238.35:8089/dc/_search?q={search_term}'
-    response = requests.get(api_url)
-
-    logger.debug(f'Gesis response status code: {response.status_code}')
-    logger.debug(f'Gesis response headers: {response.headers}')
-    
-    response = requests.get(api_url)
-    data = response.json()
 
     try:
-        hits = data.get('hits', {}).get('hits', [])
-    except AttributeError:
-        hits = []  # Set hits as an empty list if the 'get' operation fails due to AttributeError
+        # Send a GET request to the API URL
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raise an exception for non-successful response status codes
 
-    # Iterate through the hits array and extract the dc fields
-    for hit in hits:
-            dc_fields = hit['_source']['dc']
-            # Extract the desired fields from the dc object
-            if 'relation' in dc_fields:
-                doi = dc_fields['relation']['nn'][0]
-            else:
-                doi = 'Unknown DOI'
+        logger.debug(f'Gesis response status code: {response.status_code}')
+        logger.debug(f'Gesis response headers: {response.headers}')
 
-            title = dc_fields['title']['all'][0] if 'title' in dc_fields and 'all' in dc_fields['title'] else ''
-            authors = dc_fields['creator']['all'][0] if 'creator' in dc_fields and 'all' in dc_fields['creator'] else ''
-            authors_list = '; '.join(authors for authors in dc_fields['creator']['all'])
-            description = dc_fields['description']['all'][0] if 'description' in dc_fields and 'all' in dc_fields['description'] else ''
-            #extract the first five sentences from the description
-            # sentences = re.split(r'(?<=[.!?])\s+', description)
-            # short_description = ' '.join(sentences[:2])
-            short_description = utils.remove_html_tags(description)
-            type = dc_fields['type']['all'][0] if 'type' in dc_fields and 'all' in dc_fields['type'] else 'Type not available'
-            date = dc_fields['date']['nn'][0]
-            id = hit['_id']
-            id = id.replace('.', '-')
-            url = f"https://search.gesis.org/research_data/datasearch-{id}"
-           
-            if type == "Dataset":
-                results.append(
-                    Dataset(
-                            title=title, 
+        data = response.json()
+
+        try:
+            # Extract the 'hits' array from the response data
+            hits = data.get('hits', {}).get('hits', [])
+        except AttributeError:
+            hits = []  # Set hits as an empty list if the 'get' operation fails due to AttributeError
+
+        # Iterate through the hits array and extract the dc fields
+        for hit in hits:
+            try:
+                # Extract the dc fields from the hit object
+                dc_fields = hit['_source']['dc']
+
+                # Extract the desired fields from the dc object
+                doi = dc_fields['relation']['nn'][0] if 'relation' in dc_fields and 'nn' in dc_fields['relation'] else 'Unknown DOI'
+                title = dc_fields['title']['all'][0] if 'title' in dc_fields and 'all' in dc_fields['title'] else ''
+                
+                authors = ''
+                if 'creator' in dc_fields and 'all' in dc_fields['creator']:
+                    authors = dc_fields['creator']['all'][0]
+                authors_list = '; '.join(authors for authors in dc_fields['creator']['all']) if authors else ''
+                
+                description = dc_fields['description']['all'][0] if 'description' in dc_fields and 'all' in dc_fields['description'] else ''
+                short_description = utils.remove_html_tags(description)
+                type = dc_fields['type']['all'][0] if 'type' in dc_fields and 'all' in dc_fields['type'] else 'Type not available'
+                date = dc_fields['date']['nn'][0] if 'date' in dc_fields and 'nn' in dc_fields['date'] else ''
+                id = hit['_id']
+                id = id.replace('.', '-')
+                url = f"https://search.gesis.org/research_data/datasearch-{id}"
+
+                if type == "Dataset":
+                    # Create a Dataset object and append it to the results list
+                    results.append(
+                        Dataset(
+                            title=title,
                             url=url,
                             authors=authors_list,
-                            description=short_description, 
+                            description=short_description,
                             date=date
-                            
+                        )
                     )
-                )
-            elif type == "Article":
-                results.append(
-                    Article(
-                            title=title, 
+                elif type == "Article":
+                    # Create an Article object and append it to the results list
+                    results.append(
+                        Article(
+                            title=title,
                             url=url,
                             authors=authors_list,
-                            description=short_description, 
-                            date=date, 
-                            # doi=doi, 
-                            
+                            description=short_description,
+                            date=date,
+                            # doi=doi,
+                        )
                     )
-                )
-     
+            except KeyError as e:
+                # Handle the case when a key is not found in the response
+                logger.warning(f"Key not found in the response: {e}. Skipping this hit.")
+            except IndexError as e:
+                # Handle the case when an index is out of range
+                logger.warning(f"Index out of range: {e}. Skipping this hit.")
+    except requests.exceptions.RequestException as e:
+        # Handle any errors that occur while making the API request
+        logger.error(f"Error occurred while making the API request: {e}")
+    except ValueError as ve:
+        # Handle errors that occur while parsing the response JSON
+        logger.error(f"Error occurred while parsing the response JSON: {ve}")
+
     logger.info(f'Got {len(results)} records from Gesis')
--- orcid.py
+++ orcid.py
@@ -3,6 +3,7 @@ import logging
 from objects import Person
 
 def get_orcid_access_token():
+    # Function to obtain the ORCID access token using client credentials
     token_url = 'https://orcid.org/oauth/token'
     client_id = 'APP-JJ5QRCFPR76FJCER'
     client_secret = 'cc4612d8-da98-49b4-ab3e-374d824a5fb1'
@@ -31,22 +32,20 @@ def get_orcid_access_token():
 logger = logging.getLogger('nfdi_search_engine')
 
 
-#This function retrieve public information from ORCID
+# Function to search for public information from ORCID
 def search(search_term, results):
-    access_token = '45d5a287-de76-4a62-8ab9-1ffc046e7cde'
+    # It also can be used for retrieving further information, logging in, edite records etc
+    access_token = '45d5a287-de76-4a62-8ab9-1ffc046e7cde' 
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'User-Agent': 'nfdi4dsBot/1.0 (https://https://www.nfdi4datascience.de/nfdi4dsBot/; nfdi4dsBot@nfdi4datascience.de)'
     }
-    search_url = 'https://pub.orcid.org/v3.0/search?q=[SEARCH_TERM]&rows=1'
-
-    # Replace [SEARCH_TERM] with the name or search term we want to search for
-    url = search_url.replace('[SEARCH_TERM]', search_term)
+    search_url = 'https://pub.orcid.org/v3.0/search?q="' + search_term.replace(' ', '%20') + '"'
 
     try:
         # Send the GET request to search for public data
-        response = requests.get(url, headers=headers)
+        response = requests.get(search_url, headers=headers)
 
         logger.debug(f'Orcid response status code: {response.status_code}')
         logger.debug(f'Orcid response headers: {response.headers}')
@@ -57,80 +56,89 @@ def search(search_term, results):
 
             # Check if the response contains any search results
             if 'result' in json_data and isinstance(json_data['result'], list) and json_data['result']:
-                orcid_id = json_data['result'][0]['orcid-identifier']['path']
+                # Iterate through the first 10 results for now (can be changed)
+                for result in json_data['result'][:10]:  
+                    orcid_id = result['orcid-identifier']['path']
 
-                # Generate the URL to the person's public profile in ORCID
-                profile_url = f"https://orcid.org/{orcid_id}"
+                    # Generate the URL to the person's public profile in ORCID
+                    profile_url = f"https://orcid.org/{orcid_id}"
 
-                # Retrieve the public data for the found ORCID iD
-                public_data_url = f'https://pub.orcid.org/v3.0/{orcid_id}/person'
-                response = requests.get(public_data_url, headers=headers)
+                    # Retrieve the public data for the found ORCID iD
+                    public_data_url = f'https://pub.orcid.org/v3.0/{orcid_id}/person'
+                    response = requests.get(public_data_url, headers=headers)
 
-                if response.status_code == 200:
-                    # Extract the JSON response
-                    json_data = response.json()
-                    
-                    # Extract the name information
-                    name_data = json_data.get('name', {})
-                    given_names = name_data.get('given-names', {}).get('value', '')
-                    family_name = name_data.get('family-name', {}).get('value', '')
+                    if response.status_code == 200:
+                        # Extract the JSON response
+                        json_data = response.json()
+                        
+                        # Extract the name information
+                        name_data = json_data.get('name', {})
+                        given_names = name_data.get('given-names', {}).get('value', '')
 
-                    display_name = f"{given_names} {family_name}"
+                        # Handling attribute error for 'NotType' when searching for none author search terms
+                        family_name = ''
+                        if 'family-name' in name_data and name_data['family-name'] is not None:
+                            family_name = name_data.get('family-name', {}).get('value')
 
-                    # Extract email information
-                    email = json_data.get('emails', {}).get('email', [])
-                    email = email[0]['email'] if email else None
+                        full_names = f"{given_names} {family_name}"
 
-                    researcher_urls = json_data.get('researcher-urls', {}).get('researcher-url', [])
-                    keywords = json_data.get('keywords', {}).get('keyword', [])
-                    external_identifiers = json_data.get('external-identifiers', {}).get('external-identifier', [])
+                        try:
+                            display_name = name_data.get('credit-name', {}).get('value') or name_data.get('display-name', {}).get('value')
+                        except AttributeError:
+                            display_name = f"{given_names}{family_name}"
 
-                    researcher_url = [researcher_url.get('url', '') for researcher_url in researcher_urls]
-                    # print ("Research UR:", researcher_url)
+                        # Extract email information
+                        email = json_data.get('emails', {}).get('email', [])
+                        email = email[0]['email'] if email else None
 
-                    keyword = [keyword.get('content', '') for keyword in keywords]
-                    # print("Kyewords:", keyword)
+                        researcher_urls = json_data.get('researcher-urls', {}).get('researcher-url', [])
+                        keywords = json_data.get('keywords', {}).get('keyword', [])
+                        external_identifiers = json_data.get('external-identifiers', {}).get('external-identifier', [])
 
-                    external_identifier_values = []
-                    for external_identifier in external_identifiers:
-                        external_identifier_type = external_identifier.get('external-id-type', '')
-                        external_identifier_value = external_identifier.get('external-id-value', '')
-                        external_identifier_values.append((external_identifier_type, external_identifier_value))
-                    
-                    affiliations = json_data.get('employments', {}).get('employment-summary', [])
-                    if affiliations:
-                        for affiliation in affiliations:
-                            organization = affiliation.get('organization', {})
-                            name = organization.get('name', '')
-                            address = organization.get('address', {})
-                            city = address.get('city', '')
-                            region = address.get('region', '')
-                            country = address.get('country', '')
-                            # aassign the affiliation data to the affiliation_data dictionary
-                            affiliation_data = {
-                                'name': name,
-                                'address': f"{city}, {region}, {country}"
-                            }
-                            results.append(
-                                Person(
-                                    url = profile_url,
-                                    name = display_name,
-                                    affiliation = affiliation_data
+                        researcher_url = [researcher_url.get('url', '') for researcher_url in researcher_urls]
+
+                        keyword = [keyword.get('content', '') for keyword in keywords]
+
+                        external_identifier_values = []
+                        for external_identifier in external_identifiers:
+                            external_identifier_type = external_identifier.get('external-id-type', '')
+                            external_identifier_value = external_identifier.get('external-id-value', '')
+                            external_identifier_values.append((external_identifier_type, external_identifier_value))
+                        
+                        affiliations = json_data.get('employments', {}).get('employment-summary', [])
+                        if affiliations:
+                            for affiliation in affiliations:
+                                organization = affiliation.get('organization', {})
+                                name = organization.get('name', '')
+                                address = organization.get('address', {})
+                                city = address.get('city', '')
+                                region = address.get('region', '')
+                                country = address.get('country', '')
+                                # Assign the affiliation data to the affiliation_data dictionary
+                                affiliation_data = {
+                                    'name': name,
+                                    'address': f"{city}, {region}, {country}"
+                                }
+                                results.append(
+                                    Person(
+                                        url=profile_url,
+                                        name=full_names,
+                                        affiliation=affiliation_data
+                                    )
                                 )
+                        else:
+                            affiliation_data = 'No affiliations found.'
+
+                        results.append(
+                            Person(
+                                url=profile_url,
+                                name=full_names,
+                                affiliation=affiliation_data
                             )
-                    else:
-                        affiliation_data = 'No affiliations found.'
-
-                    results.append(
-                        Person(
-                        url = profile_url,
-                        name = display_name,
-                        affiliation = affiliation_data
                         )
-                    )
 
-                else:
-                    print("Failed to retrieve public data:", response.text)
+                    else:
+                        print("Failed to retrieve public data:", response.text)
             else:
                 print("No results found for the search term:", search_term)
         else:
