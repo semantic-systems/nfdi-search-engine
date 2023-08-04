3bcc00586bae2eb085f47c730504d44238b255b0
Najeeb Shams <44796050+Najeeb-Shams@users.noreply.github.com>
2023-08-04 12:15:14 +0200

    GEPRIS integrated to schema.org mapping

--- sources/gepris.py
+++ sources/gepris.py
@@ -2,10 +2,13 @@ import requests
 from objects import Gepris
 import logging
 from bs4 import BeautifulSoup
+from objects import Project, Person
+import utils
 
 logger = logging.getLogger('nfdi_search_engine')
 
 
+@utils.timeit
 def search(search_term, results):
     base_url = 'https://gepris.dfg.de/gepris/OCTOPUS'
     url = f"{base_url}?context=projekt&hitsPerPage=1&index=0&keywords_criterion={search_term}&language=en&task=doSearchSimple"
@@ -13,9 +16,15 @@ def search(search_term, results):
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for non-2xx status codes
+
+        logger.debug(f'Gepris response status code: {response.status_code}')
+        logger.debug(f'Gepris response headers: {response.headers}')
     
         soup = BeautifulSoup(response.content, 'html.parser')
         result = soup.find("span", id="result-info")
+
+        total_reocrds = result.text
+        logger.info(f'GEPRIS - {total_reocrds} records found')
         
         if result:
             url = f"{base_url}?context=projekt&hitsPerPage={result.text}&index=0&keywords_criterion={search_term}&language=en&task=doSearchSimple"
@@ -30,23 +39,36 @@ def search(search_term, results):
             
             for entry in entries:
                 try:
+                    resources = Project()
+                    resources.source = 'GEPRIS'
                     project_link = entry.find("a")["href"]
                     project_description = ''.join(entry.find("div", class_="beschreibung").find_all(text=True, recursive=False)).strip()
+                    resources.description = project_description
+                    resources.abstract = project_description
                     title = entry.find("h2").text.strip()
+                    resources.name = title
                     term = entry.find("div", class_="two_columns").find("span", class_="value2").text.strip()
+                    resources.dateLastModified = term
                     applicant_leader = entry.find("div", class_="details").find("span", class_="value").text.strip()
+                    resources.applicant =  applicant_leader
+                   
+                    
                     
                     project_url = 'https://gepris.dfg.de' + project_link + '?language=en'
+                    resources.url = project_url
                     
-                    results.append(
-                        Gepris(
-                            url=project_url,
-                            title=title,
-                            description=project_description,
-                            date=term,
-                            applicant_or_leader=applicant_leader
-                        )
-                    )
+                    # results.append(
+                    #     Gepris(
+                    #         url=project_url,
+                    #         title=title,
+                    #         description=project_description,
+                    #         date=term,
+                    #         applicant_or_leader=applicant_leader
+                    #     )
+                    # )
+
+                    results['resources'].append(resources)
+
                 except KeyError:
                     logger.warning("Key 'href' not found in 'a' tag. Skipping project.")
                 except AttributeError:
