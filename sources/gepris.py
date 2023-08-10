e9479abf1405e2c5ba3625c6d5223fd59a8c4e54
Najeeb Shams <44796050+Najeeb-Shams@users.noreply.github.com>
2023-08-10 12:47:58 +0200

    new layout's organization tab implementation and find_organization function added to GEPRIS

--- sources/gepris.py
+++ sources/gepris.py
@@ -2,7 +2,7 @@ import requests
 from objects import Gepris
 import logging
 from bs4 import BeautifulSoup
-from objects import Project, Person
+from objects import Project, Person, Organization
 import utils
 
 logger = logging.getLogger('nfdi_search_engine')
@@ -11,7 +11,9 @@ logger = logging.getLogger('nfdi_search_engine')
 @utils.timeit
 def search(search_term, results):
     # function call to retrive author or researchers from GEPRIS
-    find_authors(search_term, results)
+    find_author(search_term, results)
+    # function call to retrive organizations or institutions from GEPRIS
+    find_organization(search_term, results)
 
     base_url = 'https://gepris.dfg.de/gepris/OCTOPUS'
     url = f"{base_url}?context=projekt&hitsPerPage=1&index=0&keywords_criterion={search_term}&language=en&task=doSearchSimple"
@@ -88,7 +90,7 @@ def search(search_term, results):
         logger.error(f'An error occurred during the search: {e}')
 
 
-def find_authors(search_term, results):
+def find_author(search_term, results):
 
     base_url = 'https://gepris.dfg.de/gepris/OCTOPUS'
     url = f"{base_url}?context=person&hitsPerPage=1&index=0&keywords_criterion={search_term}&language=en&task=doSearchSimple"
@@ -135,11 +137,72 @@ def find_authors(search_term, results):
                 except KeyError:
                     logger.warning("Key 'href' not found in 'a' tag. Skipping project.")
                 except AttributeError:
-                    logger.warning("Unable to find project details. Skipping project.")
+                    logger.warning("Unable to find author for the search term")
         
         # logger.info(f'Got {len(results)} records from Gepris')
     
     except requests.exceptions.RequestException as e:
-        logger.error(f'Error occurred while making a request to Gepris: {e}')
+        logger.error(f'Error occurred while making a request to Gepris authors: {e}')
+    except Exception as e:
+        logger.error(f'An error occurred during the search: {e}')
+
+
+def find_organization(search_term, results):
+
+    base_url = 'https://gepris.dfg.de/gepris/OCTOPUS'
+    url = f"{base_url}?context=institution&hitsPerPage=1&index=0&keywords_criterion={search_term}&language=en&task=doSearchSimple"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+
+        logger.debug(f'Gepris response status code: {response.status_code}')
+        logger.debug(f'Gepris response headers: {response.headers}')
+    
+        soup = BeautifulSoup(response.content, 'html.parser')
+        result = soup.find("span", id="result-info")
+
+        total_reocrds = result.text
+        logger.info(f'GEPRIS - {total_reocrds} records found')
+        
+        if result:
+            url = f"{base_url}?context=institution&hitsPerPage={result.text}&index=0&keywords_criterion={search_term}&language=en&task=doSearchSimple"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            
+        soup = BeautifulSoup(response.content, 'html.parser')
+        organization_list = soup.find("div", id="liste")
+        try:
+            if organization_list:
+                organizations = organization_list.find_all("div", class_=["eintrag_alternate","eintrag"])
+                
+                for organization in organizations:
+                    try:
+
+                        orgObj = Organization()
+                        orgObj.source = 'GEPRIS'
+                        orgObj.identifier = organization.find("a")["href"]
+                        orgObj.url = f'https://gepris.dfg.de{orgObj.identifier}'
+                        orgObj.name = organization.find("h2").text.strip()
+                        # Find the organizations address and retrieve text after <br> tag
+                        sub_organization = organization.find("div", class_="subInstitution")
+                        if sub_organization:
+                            orgObj.address = ','.join(sub_organization.find_all("br")[0].find_next_siblings(string=True)).strip()
+                        else:
+                            orgObj.address = " "
+
+                        results['organizations'].append(orgObj)
+                    except Exception as e:
+                        logger.warning(e)
+        except KeyError:
+            logger.warning("Key not found.")
+        except AttributeError:
+            logger.warning("Unable to find organization details")
+
+        # logger.info(f'Got {len(results)} records from Gepris')
+    
+    except requests.exceptions.RequestException as e:
+        logger.error(f'Error occurred while making a request to Gepris institutions: {e}')
     except Exception as e:
         logger.error(f'An error occurred during the search: {e}')
