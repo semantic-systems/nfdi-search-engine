c7f3501e4e3bdd9ed096c6592bf26036b2a866b5
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2024-01-16 15:45:15 +0100

    update chatbot.py

--- sources/chatbot.py
+++ sources/chatbot.py
@@ -1,12 +1,29 @@
 import requests
+import json
 
-def chat(question, data):
+def read_data(file_name):
+    with open(file_name, "r") as f:
+        data = f.read()
+    return data
+
+
+def chat(question, searc_data):
     chat_results = []
     chatbot_url = 'http://0.0.0.0:5005/'
-    ping_response = requests.get(chatbot_url + 'ping')
-    ping_message = ping_response.text
-    print(ping_message)
-    body = ''
+    # ping_response = requests.get(chatbot_url + 'ping')
+    # ping_message = ping_response.text
+    search_data = read_data('../results3.json')
+    # print(search_data)
+    body = {
+        "question": "who is the coauthor of Ricardo Usbeck?",
+        "chat-history": [],
+        "search-results": search_data
+    }
+    params = {'data': body}
+
+    response = requests.post(chatbot_url + 'chat', params=params)
+    print(response.status_code)
+    print(response.text)
     # chat_response = requests.get(chatbot_url + 'chat' + body)
     # return chat_results
 
