04fd2a46d68c7c3ac6d8bb93c594088081bf4125
pumucklkopf <120374848+pumucklkopf@users.noreply.github.com>
2024-10-09 17:41:49 +0200

    separate function for retrieving displayed repos
    - added function search_displayed_resources() which takes a list of repositories dispayed on the user screen and retrieves detailed information for them.
    - refactoring

--- sources/re3data.py
+++ sources/re3data.py
@@ -10,7 +10,19 @@ from objects import thing
 
 @utils.handle_exceptions
 def search(source: str, search_term: str, results: Dict, failed_sources: List):
-    start_time = time.time()  # TODO: Retrieval takes way too long!
+    """
+    Search for repositories in the Re3Data registry based on the search term. For re3data, the returned search results
+    are only short models of repositories and do not contain detailed information, since very limited information
+    is returned by the search-term-ready API and looping over all details is too time-consuming.
+    The detailed information is retrieved separately for each (list of ) repository in search_displayed_resources().
+    :param source: the source to search (re3data)
+    :param search_term: the search term
+    :param results: the dictionary to store the search results
+    :param failed_sources: the list to store the failed
+
+    :return: None
+    """
+    start_time = time.time()
     search_url = app.config['DATA_SOURCES'][source].get('search-endpoint', '')
     search_results = data_retriever.retrieve_data(
         source=source,
@@ -28,7 +40,7 @@ def search(source: str, search_term: str, results: Dict, failed_sources: List):
         repository = Dataset(
             name=repo.get('name', ''),
             identifier=repo.get('doi', '').partition('doi.org/')[2],
-            url=repo.get('doi', ''),
+            url=repo.get('link', {}).get('@href', ''),
             source=[thing(name=source, url=repo.get('doi', ''))],
         )
         results['resources'].append(repository)
@@ -38,26 +50,50 @@ def search(source: str, search_term: str, results: Dict, failed_sources: List):
                     message=f"{source} - retrieved {counter_repository_short_models} repository short models")
     print(f"searching Re3Data overview took {time.time() - start_time:.2f} seconds to execute")
 
+@utils.handle_exceptions
+def search_displayed_resources(displayed_repos: List[Dataset], results: Dict, failed_sources: List):
+    """
+    Retrieve detailed information for each repository in the list of displayed repositories. Since a separate API call
+    is needed for each repository, this function is time-consuming, and the number of repositories should be limited.
 
-    # counter_retrieved_resources = 0
-    # for repo in repositories:
-    #     details = data_retriever.retrieve_data(
-    #         source=source,
-    #         base_url=app.config['DATA_SOURCES'][source].get('details-endpoint', ''),
-    #         search_term=repo['id'],
-    #         failed_sources=failed_sources
-    #     )
-    #
-    #     if details:
-    #         creative_work = map_repository_to_creative_work(source, repo['doi'], details)
-    #         results['resources'].append(creative_work)
-    #         counter_retrieved_resources += 1
-    #
-    # utils.log_event(type="info", message=f"{source} - retrieved {counter_retrieved_resources} repository details")
-    # print(f"searching Re3Data took {time.time() - start_time:.2f} seconds to execute")
+    :param displayed_repos: the list of repositories to retrieve detailed information for
+    :param results: the dictionary to store the search results
+    :param failed_sources: the list to store the failed
+
+    :return: None
+    """
+    start_time = time.time()
+    source_name = displayed_repos[0].source[0].name if displayed_repos else "re3data"
+
+    counter_retrieved_resources = 0
+    for repo in displayed_repos:
+        base_url = app.config['DATA_SOURCES'][source_name].get('details-endpoint', '')
+        details = data_retriever.retrieve_data(
+            source=source_name,
+            base_url=base_url,
+            search_term=repo.url.partition('https://www.re3data.org/api/beta/repository/')[2],
+            failed_sources=failed_sources
+        ) if repo.source else None
+
+        if details:
+            dataset = map_repository_to_dataset(source_name, repo.identifier, details)
+            results['resources'].append(dataset)
+            counter_retrieved_resources += 1
+
+    utils.log_event(type="info", message=f"{source_name} - retrieved {counter_retrieved_resources} repository details")
+    print(f"searching Re3Data details took {time.time() - start_time:.2f} seconds to execute")
 
 
-def map_repository_to_creative_work(source: str, doi: str, repository_details: dict) -> Dataset:
+def map_repository_to_dataset(source: str, doi: str, repository_details: dict) -> Dataset:
+    """
+    Map the detailed information of a repository to a Dataset object.
+
+    :param source: the source of the information about the repository (re3data)
+    :param doi: the DOI of the repository
+    :param repository_details: the detailed information of the repository from the API
+
+    :return: Dataset - repository in the form of a dataset object
+    """
     repository_data = repository_details.get('r3d:re3data', {}).get('r3d:repository', {})
 
     institutions = repository_data.get('r3d:institution', [])
@@ -74,32 +110,30 @@ def map_repository_to_creative_work(source: str, doi: str, repository_details: d
             location=inst.get('r3d:institutionCountry', ''),
             email=inst.get('r3d:institutionContact', ''),
             keywords=inst.get('r3d:responsibilityType', []),
-            source=[thing(name=source, url=doi)],
+            source=[thing(name=source,
+                          url='https://www.re3data.org/repository/' +
+                              repository_data.get('r3d:re3data.orgIdentifier', ""))]
         ) for inst in institutions
     ]
 
-    # TODO: Why is only one funder allowed? -- Make a list! Look in other sources!!!
     funder = [org for org in organizations if 'funding' in org.keywords]
 
     languages = repository_data.get('r3d:repositoryLanguage', [])
     if isinstance(languages, str):
         languages = [languages]
 
-    # ToDo: why was licenses not used in prev implementation? we use!
     license_names = repository_data.get('r3d:dataLicense', {})
     if isinstance(license_names, dict):
         license_names = [license_names.get('r3d:dataLicenseName', '')]
     else:
         license_names = [item.get('r3d:dataLicenseName', '') for item in license_names]
 
-    # ToDo - does ContentType match the field logic of additionalType? Could it be a list also?
     content_types = repository_data.get('r3d:contentType', [])
     if isinstance(content_types, dict):
         content_types = [content_types.get('#text', '')]
     else:
         content_types = [ct.get('#text', '') for ct in content_types]
 
-    # Todo: is this the correct way to use the genre field?  - list of subjects!
     subjects = repository_data.get('r3d:subject', [])
     if isinstance(subjects, dict):
         subjects = [subjects.get('#text', '')]
@@ -111,10 +145,12 @@ def map_repository_to_creative_work(source: str, doi: str, repository_details: d
         alternateName=get_alternate_names(repository_data.get('r3d:additionalName', {})),
         description=repository_data.get('r3d:description', {}).get('#text', ''),
         url=repository_data.get('r3d:repositoryURL', ""),
-        identifier=doi.partition('doi.org/')[2],
+        identifier=doi,
         additionalType=', '.join(content_types),
-        source=[thing(name=source, url=doi)], # ToDo: why is source not displayed on results page?  -> needs to be a thing()
-        author=organizations,  # TODO: why are organizations as authors not displayed on the results overview page?
+        source=[thing(name=source,
+                      url='https://www.re3data.org/repository/' +
+                          repository_data.get('r3d:re3data.orgIdentifier', ""))],
+        author=organizations,
         sourceOrganization=organizations[0] if organizations else None,
         funder=funder,
         inLanguage=languages,
@@ -133,5 +169,3 @@ def get_alternate_names(additional_names) -> List[str]:
     if isinstance(additional_names, dict):
         return [additional_names.get('#text', '')]
     return []
-
-# ToDo: What about the details page, is it not implemented for the resources tab? Could we just open the doi link?
